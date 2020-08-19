00d8da74ef6ca81d1739adfad68ebe39787c88b5
angela <angela.strayhorn@yahoo.com>
2020-08-19 10:18:57 -0500

    updates for api and pulldown

--- fitness_calulator.js
+++ fitness_calulator.js
@@ -1,42 +1,84 @@
-console.log("can u c me")
+
+
+var button = $('.button');
+var inputValue = $('.inputValue');
+
+
 
 $(document).on("click", "#submitButton", function(){
-   var height =  $("#enterHeight").val();
-   
+
+    var height = $('#height').val();
+    var age = $('.age').val();
+    var gender = $('.gender').val();
+    var weigth = $('.weight').val();
+
+    console.log(height);
+    getData(height, age, gender, weigth);
+
+    $('#height').empty();
+    $("input").empty();
+    $('.gender').empty();
+    $('.weight').empty();
 });
 
 // created variables for user to enter data to calculate daily calorie requirements
-var button = document.querySelector('.button')
-var inputValue = document.querySelector('.inputValue')
-var height = document.querySelector('.height')
-var age = document.querySelector('.age')
-var gender = document.querySelector('.gender')
-var weigth = document.querySelector('.weight')
+
 
 // use may select drink choices
-var drinkChoices = ["beer 12 oz", "wine 5oz", "Spirits 1.5oz"];
-var drinkCaloriesPerServing = ["132", "123", "100"]
+var drinkChoices = ["beer 12 oz", "red wine 5oz", "white wine 5oz", "Spirits 1.5oz"];
+var drinkCaloriesPerServing = ["132", "123", "123", "100"]
 
 // user enters number of drinks consumed per week according to choices
-var drinksPerWeek = prompt("how many drinks to you consume in a week")
+//var drinksPerWeek = prompt("how many drinks to you consume in a week")
 
 // number of drinks entered is multiplied by the appropiate calorie value
 
 var exerciseChoices = ["running", "swimmimg", "walking", "HIIT" ,"yoga" ,"stength training", "zumba"]
+var exerciseCaloriesPerHalfHour = ["372", "300", "180", "374", "120", "90", "210"]
 
 
-var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://fitness-calculator.p.rapidapi.com/dailycalory?heigth=180&age=25&gender=male&weigth=70",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "fitness-calculator.p.rapidapi.com",
-		"x-rapidapi-key": "0fd6027417msh0e87a8b18391847p1ad365jsn08a6f4226710"
-	}
-}
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
 
+function getData(height, age, gender, weigth){
+
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://fitness-calculator.p.rapidapi.com/dailycalory?heigth="+height+"&age="+age+"&gender="+gender+"&weigth="+weigth,
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "fitness-calculator.p.rapidapi.com",
+            "x-rapidapi-key": "0fd6027417msh0e87a8b18391847p1ad365jsn08a6f4226710"
+        }
+    }
+
+    $.ajax(settings).done(function (response) {
+    console.log(response);
+    console.log(response.data);
+    console.log(response.data.goals);
+
+    var fitnessTrackerURL = `"url": "https://fitness-calculator.p.rapidapi.com/dailycalory?heigth="+height+"&age="+age+"&gender="+gender+"&weigth="+weigth,`
+
+    /*    $.ajax({
+            url:fitnessTrackerURL
+            method: "GET"
+            .then(function (data){
+
+            })
+        })
+    $(".bmrResults").append(data)
+
+    $(".goals").append(data.goals)
+
+        //after jax call has been fired, any html manipulation goes here
+        
+        //look into es6 temperal literals 
+
+    });
+    $(document).ready(function(){
+        $('button').click(function(){
+            $('ul').toggleClass('active')
+        });
+
+   /* });
+};
